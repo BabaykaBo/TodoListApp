6e102513fa4e7547bb26145ecb80586e031a4544
zaaqq135@gmail.com <Oleh>
2024-08-04 16:50:37 +0300

    add authorization for todo

--- TodoListApp/Services/TodoService.cs
+++ TodoListApp/Services/TodoService.cs
@@ -8,9 +8,9 @@ namespace TodoListApp.Services;
 public class TodoService
 {
     private readonly ApplicationDbContext _context;
-    private readonly UserManager<IdentityUser> _userManager;
+    private readonly UserManager<ApplicationUser> _userManager;
 
-    public TodoService(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+    public TodoService(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
     {
         _context = context;
         _userManager = userManager;
